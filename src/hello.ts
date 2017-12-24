47c9daec255d707cce43c47b4dff90ccf7e3501a
singcl <24661881@qq.com>
2017-12-24 17:00:50 +0800

    tsconfig tslntconfig

---
+++ src/hello.ts
@@ -0,0 +1,15 @@
+let isDone: boolean = false;
+let decLiteral: number = 0o321;
+let namee: string = 'blob';
+let h: string = `hello`;
+let r: string = `mey name is ${name}`;
+let listing: number[] = [1, 2, 3];
+let listingg: Array<number> = [1, 2, 3];
+let x: [string, number];
+enum Color {
+    Red = 1,
+    Green = 2,
+    Blue = 4
+}
+let c: Color = Color.Blue;
+let no: any = 'eeeeee';
