062ab018205324529220472c529ff4f367b4c29c
singcl <24661881@qq.com>
2018-09-05 00:41:48 +0800

    自定义类型保护和typeof类型保护

--- src/advanced-types/union_types.ts
+++ src/advanced-types/union_types.ts
@@ -53,3 +53,21 @@ if (isFish(pet)) {
 } else {
     pet.fly();
 }
+
+function isNumber(x: any): x is number {
+    return typeof x === 'number';
+}
+
+function isString(x: any): x is string {
+    return typeof x === 'string';
+}
+
+function padLeft2(value: string, padding: string | number): string {
+    if (isNumber(padding)) {
+        return Array(padding + 1).join(' ') + value;
+    }
+    if (isString(padding)) {
+        return padding + value;
+    }
+    throw new Error(`Expected string or number, got '${padding}'.`);
+}
