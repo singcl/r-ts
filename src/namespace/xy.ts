d5c3ce7b867332aadecfca753a2300cc05d459bf
singcl <24661881@qq.com>
2018-09-12 12:32:27 +0800

    namespace in TS

--- src/namespace/xy.ts
+++ src/namespace/xy.ts
@@ -1,3 +1,7 @@
+// TypeScript 1.5里术语名已经发生了变化。
+// “内部模块”现在称做“命名空间”。 “外部模块”现在则简称为“模块”，
+// 这是为了与 ECMAScript 2015里的术语保持一致，(也就是说 module X {相当于现在推荐的写法 namespace X {) 。
+
 namespace Validation {
     export interface StringValidator {
         isAcceptable(s: string): boolean;
