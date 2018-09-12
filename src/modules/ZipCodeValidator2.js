501df7b787855df51f4953a015c7e3b418e772b9
singcl <24661881@qq.com>
2018-09-12 12:24:35 +0800

    update

--- src/modules/ZipCodeValidator2.js
+++ src/modules/ZipCodeValidator2.js
@@ -1,13 +1,14 @@
-(function(factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
         if (v !== undefined) module.exports = v;
-    } else if (typeof define === 'function' && define.amd) {
-        define(['require', 'exports'], factory);
     }
-})(function(require, exports) {
-    'use strict';
-    Object.defineProperty(exports, '__esModule', { value: true });
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
     // 类和函数声明可以直接被标记为默认导出。 标记为默认导出的类和函数的名字是可以省略的。
     class ZipCodeValidator {
         isAcceptable(s) {
