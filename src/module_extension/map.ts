2592406f96eace98203dae013dcde80d9e11109f
singcl <24661881@qq.com>
2018-09-18 14:19:13 +0800

    你不能在扩展中声明新的顶级声明－仅可以扩展模块中已经存在的声明。

--- src/module_extension/map.ts
+++ src/module_extension/map.ts
@@ -11,3 +11,6 @@ declare module './observable' {
 Observable.prototype.map = function(f) {
     // ... another exercise for the reader
 };
+
+// 当这些声明在扩展中合并时，就好像在原始位置被声明了一样。
+// 但是，你不能在扩展中声明新的顶级声明－仅可以扩展模块中已经存在的声明。
