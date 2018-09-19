8552a93b93dfd6a843f685647922357dd98fada6
singcl <24661881@qq.com>
2018-09-19 10:00:19 +0800

    利用函数重载 重载无状态函数式组件

--- src/jsx/stateness_function.tsx
+++ src/jsx/stateness_function.tsx
@@ -1,3 +1,4 @@
+// 固有元素 和 基于值的元素
 interface FooProp {
     name: string;
     X: number;
@@ -11,3 +12,23 @@ function ComponentFoo(prop: FooProp) {
 
 
 const Button = (prop: {value: string}, context: {color: string}) => <button />
+
+// 由于无状态函数组件是简单的JavaScript函数，所以我们还可以利用函数重载。
+interface ClickableProps {
+    children: JSX.Element[] | JSX.Element;
+}
+
+interface HomeProps extends ClickableProps {
+    home: JSX.Element;
+}
+
+interface SideProps extends ClickableProps {
+    side: JSX.Element | string;
+}
+
+function MainButton(props: HomeProps): JSX.Element;
+function MainButton(props: SideProps): JSX.Element;
+
+function MainButton(props: SideProps): JSX.Element {
+    //
+}
